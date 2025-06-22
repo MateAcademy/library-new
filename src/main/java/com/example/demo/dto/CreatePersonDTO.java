53b1af277c448ccea6006e8b59f2d076704e24df
Serhii Klunnyi <serhii.klunnyi@appliedmethods.biz>
2025-06-22 07:23:56 +0300

    add edits in CreatePersonDTO.java

--- src/main/java/com/example/demo/dto/CreatePersonDTO.java
+++ src/main/java/com/example/demo/dto/CreatePersonDTO.java
@@ -18,31 +18,27 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreatePersonDTO {
 
-    @NotBlank
-    @NotEmpty(message = "Name should not be empty")
+    @NotBlank(message = "Name should not be blank")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     String name;
 
-    @NotNull
-    @Min(value = 0, message = "Age should be greater then 0")
-    @Max(value = 120, message = "Age should be less then 120")
+    @NotNull(message = "Age cannot be null")
+    @Min(value = 1, message = "Age should be greater than 0")
+    @Max(value = 120, message = "Age should be less than or equal to 120")
     Integer age;
 
-    @NotEmpty(message = "Email should not be empty")
-    @Email
-    @NotBlank
+    @NotBlank(message = "Email should not be blank")
+    @Email(message = "Email should be a valid format")
     String email;
 
-    @NotBlank
-    @NotEmpty(message = "Password should not be empty")
+    @NotBlank(message = "Password should not be blank")
     String password;
 
-    @NotBlank
-    @NotEmpty(message = "Address should not be empty")
-    @Size(min = 10, max = 50, message = "Address should be between 2 and 30 characters")
+    @NotBlank(message = "Address should not be blank")
+    @Size(min = 10, max = 100, message = "Address should be between 10 and 100 characters")
     @Pattern(
-            regexp = "^[\\p{L}\\-]+, [\\p{L}\\- ]+, \\d{6}$",
-            message = "Please provide the address in the format: 'Country, City, 123456'."
+        regexp = "^[\\p{L}\\-]+, [\\p{L}\\- ]+, \\d{6}$",
+        message = "Please provide the address in the format: 'Country, City, 123456'."
     )
     String address;
 }
