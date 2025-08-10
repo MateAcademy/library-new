0ac131d7c2ec4a3dc2d322d25211d6aa82fd982d
Serhii Klunnyi <serhii.klunnyi@appliedmethods.biz>
2025-08-10 08:01:45 +0300

    add edits

--- src/main/java/com/example/demo/controller/admin/media_classification/AdminSaveDataLeafController.java
+++ src/main/java/com/example/demo/controller/admin/media_classification/AdminSaveDataLeafController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin/test-cases")
 @RequiredArgsConstructor
 public class AdminSaveDataLeafController {
+
     private final SaveDataLeafService saveDataLeafService;
 
     @PostMapping("/apply")
